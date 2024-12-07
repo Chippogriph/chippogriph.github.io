32056ddfabdc7c20a6d147c6d9e8a22629fe862c
Christoffer Persson <christoffer-persson@hotmail.se>
2024-12-07 10:04:22 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Anpassa efter din projektstruktur
+  theme: {
+    extend: {
+      colors: {
+        signatureGreen: "#2D9073", // Lägg till färgen med ett eget namn
+        hoverBlue: "#C8D5DC",
+      },
+    },
+  },
+  plugins: [],
+};
